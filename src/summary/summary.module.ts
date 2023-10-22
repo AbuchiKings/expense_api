3427c94fc2eac6c160e076bbe3ad997274ecb80e
abuchikings <abuchikingsley76@gmail.com>
2023-10-22 22:40:33 +0100

    Added summary module

--- src/summary/summary.module.ts
+++ src/summary/summary.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SummaryService } from './summary.service';
+import { SummaryController } from './summary.controller';
+import { ReportService } from 'src/report/report.service';
 
-@Module({})
+@Module({
+  providers: [SummaryService, ReportService],
+  controllers: [SummaryController]
+})
 export class SummaryModule {}
